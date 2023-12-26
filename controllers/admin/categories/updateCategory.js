97d6053263d1f775a9100288ddae1ee3e10ce33b
Mohammad Hosein Gilan Panah <korosh5775@gmail.com>
2023-12-26 20:51:22 +0330

    add comments

--- controllers/admin/categories/updateCategory.js
+++ controllers/admin/categories/updateCategory.js
@@ -1,22 +1,28 @@
-const Category = require('../../../models/categorySchema');
+// Import modules
+const Category = require("../../../models/categorySchema");
 
-const updateCategory = async (req, res, next)=>{
-    try {
-        const {categoryId} = req.params
-        const {name} = req.body;
-        const category = await Category.findById({categoryId});
+const updateCategory = async (req, res, next) => {
+  try {
+    const { categoryId } = req.params;
+    const { name } = req.body;
+    //*finding the category from database
+    const category = await Category.findById({ categoryId });
 
-        if(!category){
-            const err = new Error("there is no category to edit");
-            err.statusCode = 404 //* not fount
-            throw err;
-        }
-
-        await Category.updateOne({_id:categoryId},{$set:{name}});
-
-        res.status(200).json("category has been updated");
-    } catch (error) {
-        next(error)
+    //* checking if category exists
+    if (!category) {
+      const err = new Error("there is no category to edit");
+      err.statusCode = 404; //* not fount
+      throw err;
     }
-}
+
+    //*updating category
+    await Category.updateOne({ _id: categoryId }, { $set: { name } });
+
+    //*send successfully message
+    res.status(200).json("category has been updated");
+  } catch (error) {
+    next(error);
+  }
+};
+//export updateCategory
 module.exports = updateCategory;
